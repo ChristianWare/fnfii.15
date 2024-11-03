84ca98218a00baabc4284e685fe60a7c15dea328
Chris Ware <c.p.ware@hotmail.com>
2024-11-03 16:22:02 -0700

    Ecomm features update

--- src/components/EveryPlan/EveryPlan.tsx
+++ src/components/EveryPlan/EveryPlan.tsx
@@ -13,10 +13,35 @@ import EveryPlanImage from "../../../public/images/everyPlan.png";
 const EveryPlan = () => {
   const data = [
     {
+      icon: "",
+      title: "",
+      description: "",
+    },
+    {
+      icon: "",
+      title: "",
+      description: "",
+    },
+    {
+      icon: "",
+      title: "",
+      description: "",
+    },
+    {
       icon: <Hosting width={30} height={30} className={styles.icon} />,
       title: "Hosting",
       description:
         "We use CDN's, or content delivery networks, to speed up the delivery of web content by bringing it closer to where users are.",
+    },
+    {
+      icon: "",
+      title: "",
+      description: "",
+    },
+    {
+      icon: "",
+      title: "",
+      description: "",
     },
     {
       icon: <Edit width={30} height={30} className={styles.icon} />,
@@ -30,16 +55,15 @@ const EveryPlan = () => {
       description:
         "If you ever need to reach out to us for any reason, call us! We're here and ready to help.",
     },
+    // {
+    //   icon: "",
+    //   title: "",
+    //   description: "",
+    // },
     {
       icon: "",
       title: "",
       description: "",
-    },
-    {
-      icon: <Design width={30} height={30} className={styles.icon} />,
-      title: "Web Design/Development",
-      description:
-        "We design and develop each website we build with pure code to ensure that your website is fast and ranks high on google serch results.",
     },
     {
       icon: <Listing width={30} height={30} className={styles.icon} />,
@@ -53,31 +77,36 @@ const EveryPlan = () => {
       description:
         "We will set up your plausible analytics account so that you dont have to. Focus on your business, and we will take care of the technical stuff.",
     },
+    {
+      icon: <Design width={30} height={30} className={styles.icon} />,
+      title: "Web Design/Development",
+      description:
+        "We design and develop each website we build with pure code to ensure that your website is fast and ranks high on google serch results.",
+    },
   ];
 
   return (
     <section className={styles.container}>
       {/* <LayoutWrapper> */}
-        <div className={styles.parent}>
-          <div className={styles.top}>
-            <h2 className={`${styles.heading}`}>
-              <span className={styles.em}>Everything </span> we build
-              includes...
-            </h2>
-          </div>
-          <div className={styles.bottom}>
-            {data.map((x, index) => (
-              <div className={styles.card} key={index}>
-                {x.icon && <div className={styles.iconContainer}>{x.icon}</div>}
-                <h3 className={styles.title}>{x.title}</h3>
-                <p className={styles.desc}>{x.description}</p>
-              </div>
-            ))}
-          </div>
-          <div className={styles.imgContainer}>
-            <Image src={EveryPlanImage} alt='' fill className={styles.img} />
-          </div>
+      <div className={styles.parent}>
+        <div className={styles.top}>
+          <h2 className={`${styles.heading}`}>
+            <span className={styles.em}>Everything </span> we build includes...
+          </h2>
         </div>
+        <div className={styles.bottom}>
+          {data.map((x, index) => (
+            <div className={styles.card} key={index}>
+              {x.icon && <div className={styles.iconContainer}>{x.icon}</div>}
+              <h3 className={styles.title}>{x.title}</h3>
+              <p className={styles.desc}>{x.description}</p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src={EveryPlanImage} alt='' fill className={styles.img} />
+        </div>
+      </div>
       {/* </LayoutWrapper> */}
     </section>
   );
