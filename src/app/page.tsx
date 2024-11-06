abdf093c59c830cb1064f68c4460b6935b19d40c
Chris Ware <c.p.ware@hotmail.com>
2024-11-06 15:31:36 -0700

    post hero fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Pricing from "@/components/Pricing/Pricing";
 import Problem from "@/components/Problem/Problem";
 import Projects from "@/components/Projects/Projects";
 import Solution from "@/components/Solution/Solution";
-import Testimonials from "@/components/Testimonials/Testimonials";
+// import Testimonials from "@/components/Testimonials/Testimonials";
 import { homePageFaqs } from "../../lib/data";
 import Calendly from "@/components/Calendly/Calendly";
 import ContactHero from "@/components/ContactHero/ContactHero";
@@ -20,9 +20,9 @@ export default function Home() {
       <Problem />
       <Solution />
       <EcommFeatures />
-      <Testimonials />
-      <PostHero />
+      {/* <Testimonials /> */}
       <AboutScrollText />
+      <PostHero />
       <Projects />
       <Pricing bgColor='tan' />
       {/* <HowItWorks /> */}
--- src/components/PostHero/PostHero.tsx
+++ src/components/PostHero/PostHero.tsx
@@ -3,7 +3,7 @@ import styles from "./PostHero.module.css";
 import Payment from "../../../public/icons/payment.svg";
 import SEO from "../../../public/icons/seo.svg";
 import Design from "../../../public/icons/designii.svg";
-import Multiple from "../../../public/icons/multiple.svg";
+// import Multiple from "../../../public/icons/multiple.svg";
 import Img1 from "../../../public/images/posthero.png";
 import Image from "next/image";
 
@@ -11,27 +11,27 @@ const data = [
   {
     icon: <Payment width={75} height={75} className={styles.icon} />,
 
-    feature: "Trust & Transparency",
+    feature: "Fixed Monthly Rate",
     desc: "We prioritize trust and transparency, building a reliable and secure environment for vacation rental owners.",
   },
   {
     icon: <SEO width={75} height={75} className={styles.icon} />,
 
-    feature: "User-Friendly",
+    feature: "Lightning-Fast Delivery",
     desc: "We prioritize trust and transparency, building a reliable and secure environment for vacation rental owners.",
   },
   {
     icon: <Design width={75} height={75} className={styles.icon} />,
 
-    feature: "Growth",
+    feature: "Private Design Portal",
     desc: "We prioritize trust and transparency, building a reliable and secure environment for vacation rental owners.",
   },
-  {
-    icon: <Multiple width={75} height={75} className={styles.icon} />,
+  // {
+  //   icon: <Multiple width={75} height={75} className={styles.icon} />,
 
-    feature: "Personalization",
-    desc: "We prioritize trust and transparency, building a reliable and secure environment for vacation rental owners.",
-  },
+  //   feature: "Personalization",
+  //   desc: "We prioritize trust and transparency, building a reliable and secure environment for vacation rental owners.",
+  // },
 ];
 
 const PostHero = () => {
@@ -43,16 +43,20 @@ const PostHero = () => {
         </div>
         <div className={styles.content}>
           <div className={styles.top}>
-            <h2 className={styles.topHeading}>Peace of Mind</h2>
-            <h3 className={styles.heading}>
-             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia.
-            </h3>
+            <h2 className={styles.topHeading}>
+              Work with us for peace of mind
+            </h2>
+            {/* <h3 className={styles.heading}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab,
+              mollitia.
+            </h3> */}
           </div>
           <div className={styles.bottom}>
             {data.map((x, index) => (
               <div key={index} className={styles.box}>
                 {/* <div className={styles.iconContainer}>{x.icon}</div> */}
-                <span className={styles.index}>Step {index + 1}</span>
+                {/* <span className={styles.index}>Step {index + 1}</span> */}
+                <div className={styles.iconContainer}>{x.icon}</div>
                 <h4 className={styles.feature}>{x.feature}</h4>
                 <p className={styles.desc}>{x.desc}</p>
               </div>
