44e0e950d4b0f4f7bab8d92d52f0e264a96e9694
Chris Ware <c.p.ware@hotmail.com>
2024-11-07 00:54:05 -0700

    How it works

--- src/components/HowItWorks/HowItWorks.tsx
+++ src/components/HowItWorks/HowItWorks.tsx
@@ -3,7 +3,7 @@
 import LayoutWrapper from "../LayoutWrapper";
 import styles from "./HowItWorks.module.css";
 import Develop from "../../../public/icons/develop.svg";
-import Launch from "../../../public/icons/launch.svg";
+// import Launch from "../../../public/icons/launch.svg";
 import Consult from "../../../public/icons/consult.svg";
 import Notes from "../../../public/icons/notes.svg";
 import animationData from "../../../public/lottie/howitworks.json";
@@ -12,38 +12,26 @@ import Lottie from "lottie-react";
 const process = [
   {
     id: 59,
-    icon: <Consult className={styles.icon} />,
-    processName: "Consultation",
-    processDescription: "Understanding your vision and goals.",
+    icon: <Consult className={styles.icon} width={40} height={40} />,
+    processName: "Discovery",
+    processDescription:
+      "We kick off with a detailed questionnaire where you tell us about your business and share your vision for the project. Inspired by your answers, we create a blueprint and discuss it in an all-hands call. This guides our next steps, ensuring no time is wasted on off-target ideas.",
   },
   {
-    id: 50,
-    icon: <Notes className={styles.icon} />,
+    id: 40,
+    icon: <Notes className={styles.icon} width={40} height={40} />,
 
-    processName: "Planning",
-    processDescription: "Strategizing and designing a custom solution.",
+    processName: "Validation",
+    processDescription:
+      "Based on the previous insights, we present two visual concepts so you can choose the one that best meets your needs. After your feedback, we can either refine the chosen path or proceed with it as approved.",
   },
   {
     id: 61,
-    icon: <Develop className={styles.icon} />,
-    processName: "Development",
+    icon: <Develop className={styles.icon} width={40} height={40} />,
+    processName: "Execution & handoff",
     processDescription:
-      "Bringing your project to life with clean, efficient code.",
+      "Finally, we create a versatile visual system designed to work for the channels and formats you will use the most. The delivery—comprised of a guide and a set of essential assets—is asynchronous, so you can take your time for the final revision before the handoff.",
   },
-  {
-    id: 62,
-    icon: <Launch className={styles.icon} />,
-    processName: "Launch",
-    processDescription:
-      "Deploying your website and ensuring everything runs smoothly.",
-  },
-  // {
-  //   id: 63,
-  //   icon: <Support className={styles.icon} />,
-  //   processName: "Support",
-  //   processDescription:
-  //     "Ongoing maintenance and support to keep your site at its best.",
-  // },
 ];
 
 const HowItWorks = () => {
@@ -53,9 +41,7 @@ const HowItWorks = () => {
         <div className={styles.content}>
           <div className={styles.left}>
             <div className={styles.top}>
-              <h2 className={`${styles.heading} h2v2`}>
-                How does it work?
-              </h2>
+              <h2 className={`${styles.heading} h2v2`}>How does it work?</h2>
               <p className={styles.topText}>
                 This is our process for developing the perfect websites for your
                 brand. We Keep you in the loop 100% of the time.
@@ -66,14 +52,19 @@ const HowItWorks = () => {
             </div>
           </div>
           <div className={styles.right}>
-            {process.map((x) => (
+            {process.map((x, index) => (
               <div key={x.id} className={styles.card}>
-                <div className={styles.box}>
-                  <div className={styles.boxLeft}>{x.icon}</div>
-                  <div className={styles.boxRight}>
+                <div className={styles.cardLeft}>
+                  <div className={styles.iconContainer}>{x.icon}</div>
+                  <div className={styles.box}>
+                    <span className={styles.span}>Weeek {index + 1}</span>
                     <h3 className={styles.processName}>{x.processName}</h3>
-                    <p className={styles.copy}>{x.processDescription}</p>
                   </div>
+                </div>
+                <div className={styles.cardRight}>
+                  <p className={styles.processDescription}>
+                    {x.processDescription}
+                  </p>
                 </div>
               </div>
             ))}
