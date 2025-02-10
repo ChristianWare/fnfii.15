03fe4afb78214760b95544ad2863481086eed9d1
Chris Ware <c.p.ware@hotmail.com>
2025-02-10 14:14:56 -0700

    Services page start

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,20 @@
+import PageIntro from "@/components/PageIntro/PageIntro";
+import animationData from "../../../public/lottie/heroii.json";
+import EcommFeatures from "@/components/EcommFeatures/EcommFeatures";
+
 export default function ServicesPage() {
   return (
-    <div>ServicesPage</div>
-  )
+    <main>
+      <PageIntro
+        title='full spectrum of our'
+        title2='E-commerce Development'
+        sectionHeading='our services'
+        copy="Fonts & Footers is an e-commerce web development agency.
+                We're experts at blending the latest technology with
+                creative ideas for top-notch results."
+        animationData={animationData}
+      />
+      <EcommFeatures />
+    </main>
+  );
 }
